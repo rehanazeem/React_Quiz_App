4b5029ac8b305bfbed4ade0aebfb4bc5bf371e17
Rehan <rehanzzz1590@gmail.com>
2023-12-28 13:58:54 +0500

    first commit

---
+++ src/components/QuestionTimer.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+const QuestionTimer = ({ timeout, onTimeout }) => {
+  const [remainingTime, setRemainingTime] = useState(timeout);
+
+  useEffect(() => {
+    console.log('SET TIMEOUT');
+    setTimeout(timeout, onTimeout);
+  }, [timeout, onTimeout]);
+
+  useEffect(() => {
+    console.log('SET INTERVAL');
+    setInterval(() => {
+      setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
+    }, 100);
+  }, []);
+
+  return <progress id="question-time" max={timeout} value={remainingTime} />;
+};
+
+export default QuestionTimer;
---
+++ src/components/Quiz.jsx
@@ -0,0 +1,57 @@
+import { useCallback, useState } from "react";
+import QUESTIONS from "../questions.js";
+import QuestionTimer from "./QuestionTimer.jsx";
+import quizComplete from "../assets/quiz-complete.png";
+
+const Quiz = () => {
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  const activeQuestionIndex = userAnswers.length;
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(
+    selectedAnswer
+  ) {
+    setUserAnswers((prevAnswers) => {
+      return [...prevAnswers, selectedAnswer];
+    });
+  },
+  []);
+
+  const handleSkipAnswer = useCallback(
+    () => handleSelectAnswer(null),
+    [handleSelectAnswer]
+  );
+
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizComplete} alt="Quiz Completed" />
+        <h2>Quiz Completed</h2>
+      </div>
+    );
+  }
+
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <QuestionTimer timeout={10000} onTimeout={handleSkipAnswer} />
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {shuffledAnswers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Quiz;
